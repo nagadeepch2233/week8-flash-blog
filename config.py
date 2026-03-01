5b2736e4feab975e2cd126cd261654ba227b64d5
nagadeepch2233 <nagadeepch@gmail.com>
2026-03-01 15:52:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'blog.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 3
