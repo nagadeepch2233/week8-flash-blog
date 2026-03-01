1f2e9fce9ce12b0fc14769d251d3ca9521083293
nagadeepch2233 <nagadeepch@gmail.com>
2026-03-01 15:21:40 +0530

    Create posts forms.py

---
+++ posts forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class PostForm(FlaskForm):
+    title = StringField(
+        "Title",
+        validators=[DataRequired(), Length(min=3, max=200)]
+    )
+    content = TextAreaField(
+        "Content",
+        validators=[DataRequired()]
+    )
+    submit = SubmitField("Publish")
